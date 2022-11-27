c2a622631cd884c58fd6ce2753093540ad7939d1
Evaristopinto16 <evaristopinto16@gmail.com>
2022-11-27 13:52:12 -0300

    adicionar arquivo

---
+++ conta.js
@@ -0,0 +1,11 @@
+
+const btnIncrementar$ = document.getElementById("btn_incrementar");
+const p$ = document.getElementById("contador");
+
+
+let contador = 2;
+
+p$.innerHTML=contador;
+
+console.log(btnIncrementar$)
+console.log(p$);
